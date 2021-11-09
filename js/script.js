038e475b457160d4300997e596f57a2e2090ed0c
Khanzo <srt090@inbox.ru>
2021-11-09 18:29:23 +0600

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* alert("Hello!"); */
+/* 
+const result = confirm("Are you here?"); */
+
+const answers = [];
+
+/* answers[0] = prompt('ваше имя:', '');
+answers[1] = prompt('ваш age:', '');
+answers[2] = prompt('ваш last name:', '');
+ */
+const category = "dota";
+
+alert(`hey, ${category}`);
+
+
