8bdab65027ac10cd744ec9ddce7eef7f16e860fc
Khanzo <srt090@inbox.ru>
2021-11-13 00:43:59 +0600

    Работа с циклами

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+const NumberOfFilms = +prompt('Сколько фильмов вы просмотрели?', '');
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
+    movie: {},
     actors: {},
     genres: [],
+    count: NumberOfFilms,
     privat: false
 };
 
-const a = prompt('Последний просмотренный фильм?', ''),
-      b = prompt('Оцените его по 10 бальной шкале', ''),
-      c = prompt('Последний просмотренный фильм?', ''),
-      d = prompt('Оцените его по 10 бальной шкале', '');
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотреннных фильмов?', ''),
+          b = prompt('На сколько вы его оцениваете?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movie[a] = b;
+
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    } 
+}
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно таки мало фильмов дружище:(');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы classic зритель:)');
+} else if (personalMovieDB.count >= 30){ 
+    console.log('Вы сударь КИНОМАН!!!');
+} else {
+    console.log('error!');
+}
 
 console.log(personalMovieDB);
